d78e4c9905380bad8f1ea3cece777f0563f4ed91
Bwangari <wanderobeth@gmail.com>
2020-08-19 16:22:24 +0300

    added the jenkinsfile

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://beth:beth12345@cluster0.gra7r.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    production: '',
     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
